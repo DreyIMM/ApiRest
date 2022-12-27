ac235fdaf563083163b6dd77263d25918c497ca1
Andrey Dario <andreydario26@gmail.com>
2022-12-27 20:16:14 -0300

    API organizada por camadas

---
+++ MinhaAPICompleta/src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+    }
+}
